9c7d580769cb295aa68801771fe5b3780022aa32
Sandy Vanderbleek <sandy@Sandys-Air.attlocal.net>
2021-04-11 16:20:00 -0400

    minimal tests and description of process

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# Chart
+# controller
 
 typescript and jest demo project
 
 ## setup
 
-the project was setup like this
+the project was setup using the following commands
 
 ```
 npm init -y
@@ -29,5 +29,11 @@ npm test
 
 ```
 ts-node --files
-> import { add } from "./src/chart"
+> import { Controller } from "./src/chart"
 ```
+
+## criteria
+
+The core of the problem is storing datapoints and calculating peroids to render. I approached this in a test first manner by translating the examples into jest mock expectations and using the specifications of resolution to test a function that is responsible for that.
+
+Due to time contraints I chose to use TypeScript and leverage the provided code. Once I was able to setup a test environment using generated configs I proceeded to develop a minimal solution satisfying the tests.
--- test/test.ts
+++ test/test.ts
@@ -1,12 +1,60 @@
-import { Controller } from "../src/chart"
+import { Controller, Resolution, Datapoints } from "../src/chart"
 
 describe("Controller", () => {
-  it("Empty Cache", () => {
-    let ui = {setChartData: jest.fn()};
+  it("meets expected call pattern from examples", () => {
     let backend = {requestTemperatureData: jest.fn()};
-    let controller = new Controller(ui, backend, 946731600, 946735200);
+    let start = 946731600;
+    let end = 946735200;
+    let res:Resolution = 60;
+    let data = new Array(res).fill(1);
 
-    expect(ui.setChartData).toHaveBeenCalled();
-    expect(backend.requestTemperatureData).toHaveBeenCalled();
+    let ui = {setChartData: jest.fn()};
+
+    let controller = new Controller(ui, backend, start, end);
+    expect(backend.requestTemperatureData).toHaveBeenLastCalledWith(start, end, res);
+    expect(ui.setChartData).toHaveBeenLastCalledWith(new Array(60).fill(null));
+
+    controller.receiveTemperatureData(start, end, res, data);
+    expect(ui.setChartData).toHaveBeenLastCalledWith(data);
+
+    let setStart = 946729800;
+    controller.setStartTime(setStart);
+    expect(backend.requestTemperatureData).toHaveBeenLastCalledWith(setStart, start, res);
+    expect(ui.setChartData).toHaveBeenLastCalledWith(new Array(30).fill(null).concat(data));
+
+    let setData = new Array(60).fill(2);
+    controller.receiveTemperatureData(setStart, start, res, setData);
+    expect(ui.setChartData).toHaveBeenLastCalledWith(setData.concat(data));
+
+    let setEnd = 946738800;
+    let newRes:Resolution = 300;
+    let rolledData:Datapoints = [];
+    let newData:Datapoints = [];
+    controller.setEndTime(setEnd);
+    expect(backend.requestTemperatureData).toHaveBeenLastCalledWith(setStart, setEnd, newRes);
+    expect(ui.setChartData).toHaveBeenLastCalledWith(rolledData.concat(new Array(12).fill(null)))
+
+    controller.receiveTemperatureData(setStart, setEnd, newRes, newData);
+    expect(ui.setChartData).toHaveBeenLastCalledWith(newData);
   });
+
+  it("resoution() depends on chart range", () => {
+    let zero_hours = 0;
+    let two_hours = 60 * 60 * 2;
+    let controller = new Controller(
+      {setChartData: () => {}},
+      {requestTemperatureData: () => {}},
+      zero_hours,
+      two_hours);
+    expect(controller.resolution()).toBe(60);
+
+    let one_week = 60 * 60 * 24 * 7;
+    controller.setStartTime(two_hours);
+    controller.setEndTime(one_week);
+    expect(controller.resolution()).toBe(300);
+
+    controller.setStartTime(one_week);
+    controller.setEndTime(Number.POSITIVE_INFINITY);
+    expect(controller.resolution()).toBe(3600);
+  })
 });
